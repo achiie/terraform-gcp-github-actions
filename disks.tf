19be2c06941204142d2215f22aecd4437bcc415d
Achyut <sharma.achyut17@gmail.com>
2021-06-15 14:25:33 +0530

    gcs commit

--- disks.tf
+++ disks.tf
@@ -1,9 +1,9 @@
-resource "google_compute_disk" "vm_boot" {
-  name  = "vm-bootboot"
-  zone  = var.zones[0]
-  image = "ubuntu-2004-lts"
-  size  = 10
-  type  = "pd-ssd"
+# resource "google_compute_disk" "vm_boot" {
+#   name  = "vm-bootboot"
+#   zone  = var.zones[0]
+#   image = "ubuntu-2004-lts"
+#   size  = 10
+#   type  = "pd-ssd"
 
-  physical_block_size_bytes = 4096
-}
+#   physical_block_size_bytes = 4096
+# }
